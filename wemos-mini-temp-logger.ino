3a1bebbdda2fe12035506f1f3d97c0af8b78f94c
Sándor Balázs <sabuaai@INF.ELTE.HU>
2023-07-31 22:54:28 +0200

    Add hourly chart.

--- wemos-mini-temp-logger.ino
+++ wemos-mini-temp-logger.ino
@@ -3,13 +3,23 @@
 #include <Adafruit_SSD1306.h>
 #include <WEMOS_SHT3X.h>
 
-#define OLED_RESET 0  // GPIO0
+#define OLED_RESET 0
 Adafruit_SSD1306 display(OLED_RESET);
 SHT3X sht30(0x45);
 
 int tempStore[60];
 int humiStore[60];
 int i = 0;
+
+unsigned long startMillis;
+unsigned long currentMillis;
+const unsigned long period = 10000;
+
+unsigned long startMillis2;
+unsigned long currentMillis2;
+const unsigned long period2 = 60000;
+
+int skip = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -18,44 +28,62 @@ void setup() {
     tempStore[i] = 0;
     humiStore[i] = 0;
   }
-
+  startMillis = millis();
+  startMillis2 = millis();
 }
 
 void loop() {
-  
-  // Clear the buffer.
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setCursor(0, 0);
-  display.setTextColor(WHITE);
+  currentMillis = millis();
+  currentMillis2 = millis(); 
 
-  if(sht30.get()==0){
-    display.println("T: ");
-    display.setTextSize(2);
-    display.println(sht30.cTemp);
-    tempStore[i] = int(sht30.cTemp);
-
-    display.setTextSize(1);
-    display.println("H: ");
-    display.setTextSize(2);
-    display.println(sht30.humidity);
-    humiStore[i] = int(sht30.humidity)/2;
-
-    for(int j=0; j<60; j++) {
-      display.drawLine(j, 48, j, tempStore[j], WHITE);
-    }
-
-    if(i<60) {
-      i++;
+  if(millis() % 1000 < 10 ) {
+    Serial.println(skip);
+    if(skip == 0){
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setCursor(0, 0);
+      display.setTextColor(WHITE);
+      if(sht30.get()==0){
+        display.println("Temp: ");
+        display.setTextSize(2);
+        display.println(sht30.cTemp);
+        display.setTextSize(1);
+        display.println("Humidity: ");
+        display.setTextSize(2);
+        display.println(sht30.humidity);
+      }else {
+        display.println("Error!");
+      }
+      display.display();
     }else{
-      i = 0;
+      skip--;
     }
   }
-  else
-  {
-    display.println("Error!");
-  }
-  display.display();
-  delay(1000);
 
+  if (currentMillis - startMillis >= period) {
+    skip = 4;
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.setTextColor(WHITE);
+    display.println("HourlyTemp");
+    for(int j=0; j<60; j++) {
+      display.drawLine(j, 48, j, 48-tempStore[j], WHITE);
+    }
+    display.display();
+    startMillis = currentMillis;
+  }
+
+  if (currentMillis2 - startMillis2 >= period2) {
+    if(sht30.get()==0){
+      tempStore[i] = int(sht30.cTemp);
+      humiStore[i] = int(sht30.humidity)/2;
+      if(i<60) {
+        i++;
+      }else{
+        i = 0;
+      }
+    }
+    startMillis2 = currentMillis2;
+  }
 }
